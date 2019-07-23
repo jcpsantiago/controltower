55d02a4dd1c62045ab0fe4c6f3084acbef0c1a4a
Santiago <santiago@billie.io>
2019-07-23 21:18:33 +0200

    namespace conflicts

--- test/controltower/core_test.clj
+++ test/controltower/core_test.clj
@@ -2,27 +2,6 @@
   (:require [clojure.test :refer :all]
             [controltower.core :refer :all]))
 
-(def all-airports
-  [{:scheduled_service "no",}
-   :latitude_deg 59.969,
-   :name "Icy Bay Airport",
-   :iso_country "US",
-   :type "small_airport",
-   :municipality "Icy Bay",
-   :iso_region "US-AK",
-   :elevation_ft 50,
-   :iata_code "ICY",
-   :id 8126,
-   :ident "19AK",
-   :longitude_deg -141.662,
-   :wikipedia_link "http://en.wikipedia.org/wiki/Icy_Bay_Airport",
-   :gps_code "19AK",
-   :local_code "19AK"])
-
-(deftest iata-to-city
-  (testing "convert city from IATA code"
-    (is (= (iata->city "ICY") "Icy Bay"))))
-
 (deftest api-data
   (testing "get data from an API and retrieve only the body"
     (is (= (get-api-data "http://jsonplaceholder.typicode.com/todos/42")
