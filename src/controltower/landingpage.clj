d27e6cc814638d703345774efc61b9f82b968a7c
João Santiago <joaocpsantiago@gmail.com>
2019-10-26 12:55:20 +0200

    centered text, nicer mobile

--- src/controltower/landingpage.clj
+++ src/controltower/landingpage.clj
@@ -19,24 +19,24 @@
           [:div {:class "cf ph3"}
            [:nav {:class "w-90 w-80-ns mt4 center"}
             [:img {:src "/img/drawing.svg" :class "mw4"}]]
-           [:div {:class "w-90 w-80-ns mt5 center"}
-             [:div {:class "fl w-100 w-third-ns ph3"}
+           [:div {:class "flex flex-wrap-reverse w-90 w-80-ns mt5 center items-center"}
+             [:div {:class "fl w-100 w-third-ns mt5 mt0-ns pr3-m pr5-ns"}
                [:video {:autoplay "" :class "mw-100"}
                  [:source {:src "vid/slack_mockup.mp4" :type "video/mp4"}]]]
-             [:div {:class "fl w-100 w-two-thirds-ns pa2 ph3 mt4 mt0-ns"}
-                [:p {:class "f2 mt0 mb0" :style "font-weight:200"}
-                 "The best bot"
-                 [:p {:class "f1 mt0 mb1"} "to slack off at work"]]
-                [:p "We were tired of looking out of the window and wondering."]
-                [:p "Then did this, and our lives changed forever."]
+             [:div {:class "w-100 w-two-thirds-ns ph2 pr4-ns"}
+                [:p {:class "f3 f2-ns mt0 mb0" :style "font-weight:200"}
+                 "The best bot"]
+                [:p {:class "f2 f1-ns mt0 lh-title"} "to slack off at work"]
+                [:p {:class "mb1"} "We were tired of looking out of the window and wondering."]
+                [:p {:class "mt0 lh-copy"} "Then did this, and our lives changed forever."]
               [:a {:href "https://slack.com/oauth/authorize?scope=commands&client_id=110684212641.693659198309"}
                [:img {:alt "Add to slack" :height 40 :width 139
                       :src "https://platform.slack-edge.com/img/add_to_slack.png"
                       :srcset "https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"}]]]]
-           [:footer {:class "fl w-90 w-80-ns pv4 ph3 ph5-m ph6-l moon-gray center"}
+           [:footer {:class "fl w-100 pv4 ph3 ph5-m ph6-l moon-gray mt5"}
             [:small {:class "f6 db tc"} "©"
-             [:b {:class "ttu"} "UNLIMITED BUDGET LLC"] " (not a real company)"]
-            [:small {:class "tc mt3"}
+             [:span {:class "ttu b"} "unlimited budget llc"] " (not a real company)"]
+            [:small {:class "tc db mt3"}
              "Made with "
              [:svg {:width 14 :height 10 :viewBox "335 8 14 10"
                     :xmlns "http://www.w3.org/2000/svg"}
