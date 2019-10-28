45d75b958d676edf8a89befed6481a426fea2f85
João Santiago <joaocpsantiago@gmail.com>
2019-10-28 13:39:28 +0100

    hero with smaller screenshot

--- src/controltower/landingpage.clj
+++ src/controltower/landingpage.clj
@@ -16,12 +16,12 @@
           [:meta {:name "viewport"
                   :content "width=device-width, initial-scale=1.0"}]]
          [:body
-          [:div {:class "cf ph3"}
+          [:div {:class "ph3"}
            [:nav {:class "w-90 w-80-ns mt4 center"}
             [:img {:src "/img/drawing.svg" :class "mw4"}]]
            [:div {:class "flex flex-wrap-reverse w-90 w-80-ns mt5 center items-center"}
              [:div {:class "fl w-100 w-third-ns mt5 mt0-ns pr3-m pr5-ns"}
-               [:video {:autoplay "" :class "mw-100"}
+               [:video {:autoplay "" :class "mw-90"}
                  [:source {:src "vid/slack_mockup.mp4" :type "video/mp4"}]]]
              [:div {:class "w-100 w-two-thirds-ns ph2 pr4-ns"}
                 [:p {:class "f3 f2-ns mt0 mb0" :style "font-weight:200"}
@@ -33,9 +33,10 @@
                [:img {:alt "Add to slack" :height 40 :width 139
                       :src "https://platform.slack-edge.com/img/add_to_slack.png"
                       :srcset "https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"}]]]]
-           [:footer {:class "fl w-100 pv4 ph3 ph5-m ph6-l moon-gray mt5"}
+           [:footer {:class "w-100 pv4 ph3 ph5-m ph6-l moon-gray mt5"}
             [:small {:class "f6 db tc"} "©"
-             [:span {:class "ttu b"} "unlimited budget llc"] " (not a real company)"]
+             [:span {:class "ttu b pr1"} "unlimited budget llc"]
+             [:span {:class ""} "(not a real company, yet)"]]
             [:small {:class "tc db mt3"}
              "Made with "
              [:svg {:width 14 :height 10 :viewBox "335 8 14 10"
