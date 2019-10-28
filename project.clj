fad25d6654db2fe6310ebdbe5286e2478b33baf3
João Santiago <joaocpsantiago@gmail.com>
2019-10-28 08:01:51 +0100

    bump version 1.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jcpsantiago.controltower "1.3.0"
+(defproject jcpsantiago.controltower "1.4.0"
   :description "Slack app that tells you which flight is landing"
   :url "https://github.com/jcpsantiago/controltower"
   :license {:name "EPL-2.0"
