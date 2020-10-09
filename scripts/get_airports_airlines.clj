15bdc977b925e0949917e5153a04e15c0a7939a8
João Santiago <joaocpsantiago@gmail.com>
2020-10-09 12:17:07 +0200

    airline information from aviationstack and colors from airhex.com

--- scripts/get_airports_airlines.clj
+++ scripts/get_airports_airlines.clj
@@ -5,7 +5,8 @@
             [clojure.string :as s]
             [clojure.data.csv :as csv]
             [clojure.edn :as edn]
-            [babashka.pods :as pods]))
+            [babashka.pods :as pods]
+            [cheshire.core :as json]))
 
 
 (pods/load-pod "bootleg")
@@ -13,11 +14,96 @@
          '[pod.retrogradeorbit.hickory.select :as hs])
 
 
+;; Utility functions 
+(defn csv-data->maps [csv-data]
+  (println "Parsing csv into maps...")
+  (map zipmap
+       (->> (first csv-data) 
+            (map keyword) 
+            repeat)
+    (rest csv-data)))
+
+
+(defn code-map
+  "Nests a map using a kv from the map as the top-level keyword."
+  [target-k m]
+  (println "Nesting map using " target-k " as keyword...")
+  (let [k (-> m target-k s/lower-case keyword)]
+    {k m}))
+
+
 ;; Airline data ;;
-(def wiki-airlines 
-  (-> "https://en.wikipedia.org/wiki/List_of_airline_codes"
-      curl/get
-      :body))
+; (def wiki-airlines 
+;   (-> "https://en.wikipedia.org/wiki/List_of_airline_codes"
+;       curl/get
+;       :body))
+
+
+;; Airline colors kindly provided by AirHex.com
+;; csv file is *not* version-controlled and won't be shared
+(def airline-colors
+  (->> "../resources/airline-colors-serialized.csv"
+       slurp
+       csv/read-csv
+       (map (fn [[k v]] {(keyword (s/lower-case k)) 
+                         {:main-color (re-find #"#[0-9A-Z]{6}" v)}}))
+       (into {})))
+
+
+(def stack-api-key (System/getenv "AVIATIONSTACK_API_KEY"))
+(def stack-url "http://api.aviationstack.com/v1/airlines")
+
+(def stack-airlines
+  (let [full-url (str stack-url "?access_key=" stack-api-key "&offset=")
+        ; need to query the API first to know what's the total n
+        first-url (str full-url 0)
+        first-res (-> first-url curl/get :body (json/parse-string true))
+        first-data (:data first-res)
+        offset-seq (as-> first-res $
+                         (:pagination $)
+                         (:total $)
+                         (range 1 $ 100)
+                         (drop 1 $))
+        url-seq (map #(str full-url %) offset-seq)]
+    (reduce (fn [p c]
+             (let [res (-> c 
+                           curl/get 
+                           :body 
+                           (json/parse-string true) 
+                           :data)] 
+              (println c)
+              (reduce conj p res)))
+            first-data
+            url-seq)))
+
+
+;; Can't use the API more than 500 calls/month
+(->> stack-airlines
+     pr-str
+     (spit "./stack-airlines-dump.edn"))
+
+
+(defn merge-maps 
+  "Merges two nested maps with the same top-level keyword."
+  [mapy [k v]]
+  {k (conj v (k mapy))})
+  
+(def merge-colors (partial merge-maps airline-colors))
+
+
+(def airlines-icao (->> stack-airlines 
+                      (filter #(and 
+                                 (seq (:icao_code %)) 
+                                 (= (:status %) "active")))
+                      (map #(code-map :icao_code %)) 
+                      (filter #(contains? airline-colors (first (keys %)))) 
+                      (into {})
+                      (map merge-colors)))
+
+
+(->> airlines-icao
+     pr-str
+     (spit "../resources/airlines_icao.edn"))
 
 
 ; https://gist.github.com/jackrusher/97734e71bb748ed9c263d6e3daea2b38
@@ -89,19 +175,13 @@
 (def complete-iata? (partial complete? :iata))
 
 
-(defn airline-code-map
-  [k airline-map]
-  (let [code (->> airline-map
-                  (mapv #(keyword (s/lower-case (k %)))))]
-    (zipmap code airline-map)))
-
-
-(println "Collecting Airline ICAO codes")
-(->> (html->map wiki-airlines)
-     (filter (every-pred complete-iata? complete-icao? not-defunct?)) 
-     (airline-code-map :icao)
-     pr-str
-     (spit "../resources/airlines_icao.edn"))
+; FIXME: remove this, not needed anymore since using aviationstack
+;(println "Collecting Airline ICAO codes")
+;(->> (html->map wiki-airlines)
+;     (filter (every-pred complete-iata? complete-icao? not-defunct?)) 
+;     (code-map :icao)
+;     pr-str
+;     (spit "../resources/airlines_icao.edn"))
 
 
 ;; Airport data ;;
@@ -112,19 +192,10 @@
       :body))
 
 
-(defn csv-data->maps [csv-data]
-  (println "Parsing csv into maps...")
-  (map zipmap
-       (->> (first csv-data) 
-            (map keyword) 
-            repeat)
-    (rest csv-data)))
-
-
 (defn airports-to-keep [airports-map] 
   (println "Keeping airports with scheduled services...")
   (->> airports-map
-       ; only scheduled flights for now so no helipads sorry : (
+       ;; only scheduled flights for now so no helipads sorry : (
        (filter #(and (seq (:iata_code %)) 
                      (= (:scheduled_service %) "yes")))
        (pmap #(select-keys % [:type :name :latitude_deg :longitude_deg :elevation_ft
@@ -176,7 +247,7 @@
      airports-to-keep
      checkpoint-bounding-boxes
      (pmap maxmin-lonlat)
-     (airline-code-map :iata_code)
+     (code-map :iata_code)
      pr-str
      (spit "../resources/airports_with_boxes.edn"))
 
